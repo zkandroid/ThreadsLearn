78b3d305d09d558ca940de90800391b18cb758eb
末日声萧 <wuqiuashuiisme@gmail.com>
2018-07-14 17:50:45 +0800

    ThreadPool 2

--- src/main/java/ThreadPool.java
+++ src/main/java/ThreadPool.java
@@ -1,4 +1,6 @@
  import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -9,19 +11,28 @@ import java.util.concurrent.TimeUnit;
 //如果当前线程池中的线程数目达到maximumPoolSize，则会采取任务拒绝策略进行处理；
 //如果线程池中的线程数量大于 corePoolSize时，如果某线程空闲时间超过keepAliveTime，线程将被终止，
 //直至线程池中的线程数目不大于corePoolSize；如果允许为核心池中的线程设置存活时间，那么核心池中的线程空闲时间超过keepAliveTime，线程也会被终止。
+//从它们的具体实现来看，它们实际上也是调用了ThreadPoolExecutor，只不过参数都已配置好了。
+
+//newFixedThreadPool创建的线程池corePoolSize和maximumPoolSize值是相等的，它使用的LinkedBlockingQueue；
+
+//newSingleThreadExecutor将corePoolSize和maximumPoolSize都设置为1，也使用的LinkedBlockingQueue；
+
+//newCachedThreadPool将corePoolSize设置为0，将maximumPoolSize设置为Integer.MAX_VALUE，使用的SynchronousQueue，也就是说来了任务就创建线程运行，当线程空闲超过60秒，就销毁线程
 //https://www.cnblogs.com/dolphin0520/p/3932921.html
 public class ThreadPool {
 	
 	
 	public static void main(String [] args) {
-		ThreadPoolExecutor tPoolExecutor = new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(5));
+		//ThreadPoolExecutor tPoolExecutor = new ThreadPoolExecutor(5, 10, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(5));
 
-		
+		//ExecutorService tPoolExecutor = Executors.newFixedThreadPool(5);
+		//ExecutorService tPoolExecutor=Executors.newSingleThreadExecutor();
+		ExecutorService tPoolExecutor=Executors.newCachedThreadPool();
 		for(int i=0;i<20;i++){
 	        MyTask myTask = new MyTask(i);
 	        tPoolExecutor.execute(myTask);
-	        System.out.println("线程池中线程数目："+tPoolExecutor.getPoolSize()+"，队列中等待执行的任务数目："+
-	        		tPoolExecutor.getQueue().size()+"，已执行玩别的任务数目："+tPoolExecutor.getCompletedTaskCount());
+	       // System.out.println("线程池中线程数目："+tPoolExecutor.getPoolSize()+"，队列中等待执行的任务数目："+
+	        //		tPoolExecutor.getQueue().size()+"，已执行玩别的任务数目："+tPoolExecutor.getCompletedTaskCount());
 	    }
 		tPoolExecutor.shutdown();
 	}
